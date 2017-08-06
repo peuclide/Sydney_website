e5f205b2aaa9a954f8e5e4b52c960f23319bf19c
peuclide <peuclide@uvm.edu>
2017-08-06 10:45:38 -0400

    now with _yml corrected?

--- build_site.R
+++ build_site.R
@@ -2,6 +2,6 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("~/Documents/OneDrive/Rmd_Sydney_website")
+setwd("~/Documents/OneDrive/Rmd_Sydney_website/Sydney_website")
 #render your sweet site. 
 rmarkdown::render_site()
